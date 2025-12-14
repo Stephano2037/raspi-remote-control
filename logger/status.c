65c05c8059a2b918694be302d9a4bcab4f190266
stephano <stephano2037@gmail.com>
2025-12-14 12:18:48 +0900

    rp-remote #7: now functionsacting on  multithread * get disk & memory information in one function (thread_disk_and_mem) ;

--- logger/status.c
+++ logger/status.c
@@ -44,24 +44,66 @@ typedef struct {
 }SystemStatus;
 
 // /proc/meminfo 파일 파싱해서 메모리 정보 가져오기
-int get_memory_info(MemInfo *mem) {
+void *thread_mem_and_disk(void *arg) {
+//int get_memory_info(MemInfo *mem) {
+    SystemStatus *status = (SystemStatus*) arg;
+    
+    MemInfo mem={0,};  
+    struct statvfs stat={0,};
+   
+    //1. memory
     FILE *fp = fopen("/proc/meminfo", "r");
     if (fp == NULL) {
         perror("fopen /proc/meminfo");
-        return -1;
+        status->mem_percent = -1.0f;
     }
 
-    char line[256]={0,};
+    unsigned long total = 0, available = 0;
+
+    char line[MAXBUFFER]={0,};
     while (fgets(line, sizeof(line), fp)) {
-        if (sscanf(line, "MemTotal: %ld kB", &mem->total) == 1) continue;
-        if (sscanf(line, "MemFree: %ld kB", &mem->free) == 1) continue;
-        if (sscanf(line, "MemAvailable: %ld kB", &mem->available) == 1) continue;
-        if (sscanf(line, "Buffers: %ld kB", &mem->buffers) == 1) continue;
-        if (sscanf(line, "Cached: %ld kB", &mem->cached) == 1) continue;
+        if (sscanf(line, "MemTotal: %ld kB", &mem.total) == 1) continue;
+        if (sscanf(line, "MemFree: %ld kB", &mem.free) == 1) continue;
+        //mem available 이 없는 구형 커널도 추후 염두해야함
+        if (sscanf(line, "MemAvailable: %ld kB", &mem.available) == 1) continue;
+        if (sscanf(line, "Buffers: %ld kB", &mem.buffers) == 1) continue;
+        if (sscanf(line, "Cached: %ld kB", &mem.cached) == 1) continue;
     }
 
     fclose(fp);
-    return 0;
+
+    // 메모리 정보 확인
+    if (mem.total > 0) {
+        status->mem_used_mb = (mem.total - mem.available) / 1024;
+        status->mem_total_mb =  mem.total / 1024;
+        status->mem_percent = 100.0f * ((float)(mem.total - mem.available)/(float)mem.total);
+    } else {
+        status->mem_percent = -1.0f;
+    }
+    
+    //2. disk 
+    if (statvfs("/", &stat) != 0) {
+        perror("statvfs");
+        return NULL;
+    }
+
+    // 64비트 정수로 계산  (오버플로 방지)
+    unsigned long long total_bytes =
+        (unsigned long long)stat.f_blocks * (unsigned long long)stat.f_frsize;
+    unsigned long long free_bytes =
+        (unsigned long long)stat.f_bfree  * (unsigned long long)stat.f_frsize;
+    unsigned long long used_bytes = total_bytes - free_bytes;
+
+    if (total_bytes > 0) {
+        status->disk_total_gb = (long)(total_bytes / (1024ULL * 1024ULL * 1024ULL));
+        status->disk_used_gb = (long)(used_bytes  / (1024ULL * 1024ULL * 1024ULL));
+        status->disk_percent = 100.0f * ((float)used_bytes/ (float) total_bytes) ;
+    }
+    else {
+        status->disk_percent = -1.0f;
+    }
+
+    return NULL;
 }
 
 // /proc/stat 파일 파싱해서 CPU 시간 정보 가져오기
@@ -138,69 +180,58 @@ int get_disk_usage(float *percent, long *used_gb, long *total_gb) {
 
 
 // 라즈베리파이 온도 가져오기
-float get_temperature(void) {
+void* thread_temperature(void* arg) {
+//float get_temperature(void) {
     // 라즈베리파이의 온도 센서 파일
+    SystemStatus *status = (SystemStatus*) arg;
+
     FILE *fp = fopen("/sys/class/thermal/thermal_zone0/temp", "r");
     if (fp == NULL) {
         // 이 파일이 없으면 일반 리눅스일 수 있으므로 에러 대신 -1 반환
-        return -1.0f; 
+        status->temp = -1.0f;
+        return NULL;
     }
 
-    int temp_milli_c;
+    int temp_milli_c= 0;
     fscanf(fp, "%d", &temp_milli_c);
     fclose(fp);
-
-    return (float)temp_milli_c / 1000.0;
+    
+    status->temp = (float)temp_milli_c / 1000.0;
+    return NULL;
 }
 
 int main() {
     printf("===== Raspberry Pi Status =====\n");
 
-    pthread_t t1=0;
+    pthread_t t1,t2,t3=0;
     SystemStatus status={0,}; 
 
     pthread_create(&t1,NULL,thread_cpu,&status);
+    pthread_create(&t2,NULL,thread_mem_and_disk,&status);
+    pthread_create(&t2,NULL,thread_temperature,&status);
 
-    // 1. CPU
     pthread_join(t1,NULL);
+    pthread_join(t2,NULL);
+    pthread_join(t3,NULL);
     
+    // 1. CPU
     if(status.cpu_usage>0) printf("CPU Usage: %1.f%%\n",status.cpu_usage);
     else printf("CPU Usage: N/A\n");
 
-    // float cpu_usage = calculate_cpu_usage();
-    // if (cpu_usage >= 0) {
-    //     printf("CPU Usage : %.1f%%\n", cpu_usage);
-    // } else {
-    //     printf("CPU Usage : N/A\n");
-    // }
-
     // 2. Memory
-    MemInfo mem;
-    if (get_memory_info(&mem) == 0) {
-        long used_mb = (mem.total - mem.available) / 1024;
-        long total_mb = mem.total / 1024;
-        float percent = 100.0f * used_mb / total_mb;
-        printf("Memory    : %ld MB / %ld MB (%.1f%%)\n", used_mb, total_mb, percent);
-    } else {
-        printf("Memory    : N/A\n");
-    }
-
+    if(status.mem_total_mb > 0) printf("Mem Usage: : %ld MB / %ld MB ( %1.f%%)\n",
+    status.mem_used_mb , status.mem_total_mb, status.mem_percent);
+    else printf("Mem Usage: N/A\n");
+    
     // 3. Disk
-    float disk_percent=0.f;
-    long used_gb = 0.f, total_gb=0.f;
-    if (get_disk_usage(&disk_percent, &used_gb, &total_gb) == 0) {
-        printf("Disk (/)  : %ld GB / %ld GB (%.1f%%)\n", used_gb, total_gb, disk_percent);
-    } else {
-        printf("Disk (/)  : N/A\n");
-    }
+    if(status.disk_total_gb > 0 ) printf("Disk (/)  : %ld GB / %ld GB (%.1f%%)\n", 
+    status.disk_used_gb, status.disk_total_gb, status.disk_percent);
+    else printf("Disk Usage: N/A");
 
     // 4. Temperature
-    float temp = get_temperature();
-    if (temp >= 0) {
-        printf("Temp      : %.1f 'C\n", temp);
-    } else {
-        // 온도를 못 읽는 경우 (라즈베리파이가 아닐 때)는 출력 안 함
-    }
+    if (status.temp >= 0) {
+        printf("Temp      : %.1f 'C\n", status.temp);
+    } else {printf("temp N/A\n");}
     
     printf("===============================\n");
 
